7ea3a8723996d7864b85c83194dcf675aa5b28e7
Alireza <98854412+AlirezaPourchali@users.noreply.github.com>
2023-10-27 17:04:41 +0330

    fix: install with wget

--- 01.installation.sh
+++ 01.installation.sh
@@ -3,21 +3,14 @@
 # install percona Server for MySQL
 # interactie shell at the end to give your password
 
-sudo apt update
-
-curl -O https://repo.percona.com/apt/percona-release_1.0-27.generic_all.deb
-
-sudo apt install gnupg2 lsb-release ./percona-release_1.0-27.generic_all.deb -y
-
-sudo apt update
-
-sudo percona-release setup ps80
-
-sudo apt install percona-server-server -y
+wget https://downloads.percona.com/downloads/Percona-Server-8.0/Percona-Server-8.0.31-23/binary/debian/buster/x86_64/Percona-Server-8.0.31-23-r71449379-buster-x86_64-bundle.tar
+tar xvf Percona-Server-8.0.31-23-r71449379-buster-x86_64-bundle.tar
+sudo dpkg -i *.deb
 
 # install percona Xtrabackup 
 
-sudo apt install percona-xtrabackup-80 -y
+wget https://downloads.percona.com/downloads/Percona-XtraBackup-LATEST/Percona-XtraBackup-8.0.26-18/binary/debian/focal/x86_64/percona-xtrabackup-80_8.0.26-18-1.focal_amd64.deb
+sudo dpkg -i percona-xtrabackup-80_8.0.26-18-1.focal_amd64.deb
 
 sudo apt install zstd lz4 -y 
 
